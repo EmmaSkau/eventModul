51cf7874eb8d24b0f9088794711b807ac9d9723f
EmmaSkau <emmaskaujensen@gmail.com>
2025-11-03 11:11:38 +0100

    Oprettelse af filer

---
+++ src/webparts/eventModul/components/IEventModulProps.ts
@@ -0,0 +1,7 @@
+export interface IEventModulProps {
+  description: string;
+  isDarkTheme: boolean;
+  environmentMessage: string;
+  hasTeamsContext: boolean;
+  userDisplayName: string;
+}
---
+++ src/webparts/eventModul/loc/mystrings.d.ts
@@ -0,0 +1,19 @@
+declare interface IEventModulWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppLocalEnvironmentOffice: string;
+  AppLocalEnvironmentOutlook: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+  AppOfficeEnvironment: string;
+  AppOutlookEnvironment: string;
+  UnknownEnvironment: string;
+}
+
+declare module 'EventModulWebPartStrings' {
+  const strings: IEventModulWebPartStrings;
+  export = strings;
+}
